643d0ee8ec6cd266606e12e62c631f5a580adab7
Tawfek <tawfik.d@spacetoon.com>
2022-09-07 22:51:24 +0300

    update on template name , JS changes

--- Controller/Index/Configure.php
+++ Controller/Index/Configure.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (c) 2020 Tawfek Daghistani - ConfigureTech
+ * Copyright (c) 2022 Tawfek Daghistani - ConfigureTech
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -36,6 +36,7 @@ use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\NotFoundException;
+use Magento\Framework\Exception\InputException;
 use Magento\Framework\Phrase;
 use Magento\Framework\View\Result\PageFactory;
 use Psr\Log\LoggerInterface;
@@ -131,89 +132,102 @@ class Configure extends Action implements CsrfAwareActionInterface
      */
     public function execute()
     {
-        if ($this->getRequest()->isPost()) {
-            /// we need to validate the code before accepting it , we haven't agreed on a method yet
-            $cart                     = $this->cart;
-            $request                  = $this->getRequest();
-            $params                   = $request->getParams();
-            $products                 = $params["description"];
-            $wholesale                = $params["wholesale"];
-            $retail                   = $params["retail"];
-            $part_number              = $params["part_number"];
-            $mfr_part_number          = $params["mfr_part_number"];
-            $weight                   = $params["weight"];
-            $product_id               = $params["product_id"];
-            // re-add timestamp for Lloyd Mats Store , its optional
-            $timestamp                = isset($params["timestamp"]) ? $params["timestamp"] : "";
-
-            $product                  = $this->productRepository->getById($product_id);
-            $configuretech_purchase_sku = $product->getData("configuretech_purchase_product");
-            if ($configuretech_purchase_sku) {
-                $product = $product->reset()->load($product->getIdBySku($configuretech_purchase_sku));
-            } else {
-                $this->logger->error($product->getId() . " has a missing attributes [configuretech_purchase_product]");
-                throw new NotFoundException(new Phrase("System Error - this product has missing attributes , please contact support"));
-            }
-
-            if (is_null($product) || ($product->getStatus() == Status::STATUS_DISABLED)) {
-                $this->messageManager->addErrorMessage(
-                    $this->escaper->escapeHtml("We're unable to add this product to your cart, it might be disabled ,  Please try again later or contact support.")
-                );
-                return $this->_redirect($this->_redirect->getRefererUrl());
-            }
-
-            $formatted_products = [];
-            $i = 0;
-            foreach ($products as $key => $p) {
-                $formatted_products[$i]["description"]               = $p;
-                $formatted_products[$i]["wholesale"]                 = $wholesale[$key];
-                $formatted_products[$i]["retail"]                    = $retail[$key];
-                $formatted_products[$i]["part_number"]               = $part_number[$key];
-                $formatted_products[$i]["mfr_part_number"]           = $mfr_part_number[$key];
-                $formatted_products[$i]["weight"]                    = $weight[$key];
-                $formatted_products[$i]["total"]                     = $retail[$key];
-                // re-add timestamp for Lloyd Mats Store
-                $formatted_products[$i]["timestamp"]                 = $timestamp;
-                $i++;
-            }
-
-            // match the product's custom options with the data in the post-back
-            $options = [];
-            foreach ($product->getOptions() as $o) {
-                if ($o->getType() != "field") {
-                    continue;
-                }
-                $options[$o->getOptionId()] = str_replace(" ", "_", strtolower($o->getTitle()));
-            }
-            $product_params = [];
-            foreach ($formatted_products as $selected_product) {
-                foreach ($options as $option_id => $option_title) {
-                    if (isset($selected_product[$option_title])) {
-                        $product_params['options'][$option_id] = preg_replace('#<br\s*/?>#i', "\n", urldecode($selected_product[$option_title]));
-                    }
-                }
-                $cart->addProduct($product, $product_params);
-                $product_params = [];
-            }
-
-            $cart->save();
-            $this->session->setCartWasUpdated(true);
-            $this->_eventManager->dispatch(
-                'checkout_cart_add_product_complete',
-                [
-                    'product' => $product,
-                    'request' => $this->getRequest(),
-                    'response' => $this->getResponse()
-                ]
-            );
-
-            $this->messageManager->addSuccessMessage(
-                $this->escaper->escapeHtml("the selected product has been added to your cart")
-            );
-            return $this->_redirect('checkout/cart');
-        } else {
+        if (!$this->getRequest()->isPost()) {
             $this->logger->error("Page wasn't accessed with POST request");
             throw new NotFoundException(new Phrase("Method not allowed!"));
         }
+        /// we need to validate the code before accepting it , we haven't agreed on a method yet
+        // @todo: rewrite to a better validator 
+        if(
+            !isset($params["product_id"]) ||
+            !isset($params["description"]) ||
+            !isset($params["wholesale"]) ||
+            !isset($params["retail"]) ||
+            !isset($params["part_number"]) ||
+            !isset($params["mfr_part_number"]) ||
+            !isset($params["weight"])
+        ){
+            $this->logger->error("posted data: ". json_encode($params));
+            throw new InputException(new Phrase("Missing Paramters!, check the log to see all posted data"));
+        }
+        $cart                     = $this->cart;
+        $request                  = $this->getRequest();
+        $params                   = $request->getParams();
+        $products                 = $params["description"];
+        $wholesale                = $params["wholesale"];
+        $retail                   = $params["retail"];
+        $part_number              = $params["part_number"];
+        $mfr_part_number          = $params["mfr_part_number"];
+        $weight                   = $params["weight"];
+        $product_id               = $params["product_id"];
+        
+        // re-add timestamp for Lloyd Mats Store , its optional
+        $timestamp                = isset($params["timestamp"]) ? $params["timestamp"] : "";
+
+        $product                  = $this->productRepository->getById($product_id);
+        $configuretech_purchase_sku = $product->getData("configuretech_purchase_product");
+        if ($configuretech_purchase_sku) {
+            $product = $product->reset()->load($product->getIdBySku($configuretech_purchase_sku));
+        } else {
+            $this->logger->error($product->getId() . " has a missing attributes [configuretech_purchase_product]");
+            throw new NotFoundException(new Phrase("System Error - this product has missing attributes , please contact support"));
+        }
+
+        if (is_null($product) || ($product->getStatus() == Status::STATUS_DISABLED)) {
+            $this->messageManager->addErrorMessage(
+                $this->escaper->escapeHtml("We're unable to add this product to your cart, it might be disabled ,  Please try again later or contact support.")
+            );
+            return $this->_redirect($this->_redirect->getRefererUrl());
+        }
+
+        $formatted_products = [];
+        $i = 0;
+        foreach ($products as $key => $p) {
+            $formatted_products[$i]["description"]               = $p;
+            $formatted_products[$i]["wholesale"]                 = $wholesale[$key];
+            $formatted_products[$i]["retail"]                    = $retail[$key];
+            $formatted_products[$i]["part_number"]               = $part_number[$key];
+            $formatted_products[$i]["mfr_part_number"]           = $mfr_part_number[$key];
+            $formatted_products[$i]["weight"]                    = $weight[$key];
+            $formatted_products[$i]["total"]                     = $retail[$key];
+            // re-add timestamp for Lloyd Mats Store
+            $formatted_products[$i]["timestamp"]                 = $timestamp;
+            $i++;
+        }
+
+        // match the product's custom options with the data in the post-back
+        $options = [];
+        foreach ($product->getOptions() as $o) {
+            if ($o->getType() != "field") {
+                continue;
+            }
+            $options[$o->getOptionId()] = str_replace(" ", "_", strtolower($o->getTitle()));
+        }
+        $product_params = [];
+        foreach ($formatted_products as $selected_product) {
+            foreach ($options as $option_id => $option_title) {
+                if (isset($selected_product[$option_title])) {
+                    $product_params['options'][$option_id] = preg_replace('#<br\s*/?>#i', "\n", urldecode($selected_product[$option_title]));
+                }
+            }
+            $cart->addProduct($product, $product_params);
+            $product_params = [];
+        }
+
+        $cart->save();
+        $this->session->setCartWasUpdated(true);
+        $this->_eventManager->dispatch(
+            'checkout_cart_add_product_complete',
+            [
+                'product' => $product,
+                'request' => $this->getRequest(),
+                'response' => $this->getResponse()
+            ]
+        );
+
+        $this->messageManager->addSuccessMessage(
+            $this->escaper->escapeHtml("the selected product has been added to your cart")
+        );
+        return $this->_redirect('checkout/cart');      
     }
 }
